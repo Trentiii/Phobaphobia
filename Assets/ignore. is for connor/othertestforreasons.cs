80421c65bcc59e66d1a66c2875f2a77b168bf30b
tehcakeisalie <1707239@mukilteo.wednet.edu>
2021-04-29 08:26:33 -0700

    long explanation, but I did things

---
+++ Assets/ignore. is for connor/othertestforreasons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class othertestforreasons : MonoBehaviour
+{
+    public testforreasons script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > script.nextshieldTime)
+        {
+            if (Input.GetButtonUp("Fire1"))
+            {
+                Debug.Log("Shield is in!");
+                script.nextshieldTime = Time.time + script.cooldownTime;
+            }
+        }
+    }
+}
---
+++ Assets/ignore. is for connor/testforreasons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testforreasons : MonoBehaviour
+{
+
+    public float cooldownTime = 2;
+    public float nextshieldTime = 0;
+    public bool shieldout = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextshieldTime)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Debug.Log("Shield is out!");
+                //nextshieldTime = Time.time + cooldownTime;
+                shieldout = true;
+            }
+
+        }
+
+
+    }
+
+
+
+}
