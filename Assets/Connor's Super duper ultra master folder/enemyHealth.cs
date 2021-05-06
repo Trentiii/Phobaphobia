e4879e771a95678d7a42cd092db14b13ec9d0ead
tehcakeisalie <1707239@mukilteo.wednet.edu>
2021-05-06 09:23:53 -0700

    enemies can die now

---
+++ Assets/Connor's Super duper ultra master folder/enemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float enemyhealth = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyhealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.CompareTag("testingboolet"))
+        {
+            enemyhealth -= 50;
+        }
+    }
+}
