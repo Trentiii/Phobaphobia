cbf34e6fd12a45439bf94fdd451c25fa942d2646
1707235 <1707235@SC-VDGD-6QJ6853.local>
2021-05-10 08:29:51 -0700

    Update Istolethis.unity, Option_nuclear.unity, and 8 more files...

--- Assets/Thomas/Scripts/Health.cs
+++ Assets/Thomas/Scripts/Health.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mov : MonoBehaviour
+public class Health : MonoBehaviour
 {
+    public int maxHealth;
+    public int currenHealth;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currenHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
+
 }
