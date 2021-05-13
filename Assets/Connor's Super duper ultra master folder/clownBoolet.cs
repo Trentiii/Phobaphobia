d76c80dccd759186421066c1b8cbf199bdf8acc0
tehcakeisalie <1707239@mukilteo.wednet.edu>
2021-05-13 09:08:25 -0700

    Update boolet.prefab, clownBoolet.cs, and 7 more files...

---
+++ Assets/Connor's Super duper ultra master folder/clownBoolet.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clownBoolet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    Vector3 lastVelocity;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        rb.AddForce(new Vector2(-15f * 25f, 0f * 25f));
+        lastVelocity = rb.velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        var speed = lastVelocity.magnitude;
+        var direction = Vector3.Reflect(lastVelocity.normalized, coll.contacts[0].normal);
+
+        rb.velocity = direction * Mathf.Max(speed, 0f);
+
+        if (coll.gameObject.CompareTag("testingboolet"))
+        {
+            Destroy(gameObject);
+        }
+        if (coll.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D trigg)
+    {
+        if (trigg.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+        if (trigg.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Connor's Super duper ultra master folder/clownShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clownShoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject boolet;
+    public float fireRate;
+    public float cooldownTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > fireRate)
+        {
+            shoot();
+            fireRate = Time.time + cooldownTime;
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(boolet, firePoint.position, firePoint.rotation);
+    }
+}
