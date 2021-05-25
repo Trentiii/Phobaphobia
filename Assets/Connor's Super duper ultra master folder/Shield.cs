344f5611570e1202b6ad05ad3beeb81d9d940cca
1707235 <1707235@SC-VDGD-6QJ6853.local>
2021-05-25 08:23:14 -0700

    Update Shield.cs and Shield.cs.meta

---
+++ Assets/Connor's Super duper ultra master folder/Shield.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public Transform shieldpoint;
+    public GameObject shieldprototype;
+    public bool shieldout = false;
+    public float cooldownTime = 2f;
+    public float nextshieldTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextshieldTime)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                startShield();
+            }
+
+        }
+
+
+    }
+
+
+    void startShield()
+    {
+        //Instantiate(shieldprototype, shieldpoint.position, shieldpoint.rotation);
+        GameObject go = Instantiate(shieldprototype, shieldpoint.position, shieldpoint.rotation);
+        go.transform.parent = GameObject.Find("shieldpoint").transform;
+        shieldout = true;
+    }
+
+
+
+}
